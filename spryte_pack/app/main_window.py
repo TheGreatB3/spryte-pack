276b42430dbf08674e1dd59fafaaddd27820ee33
Briggs <b3apollo13@gmail.com>
2020-01-24 15:25:58 -0600

    Added image drawing

--- spryte_pack/app/main_window.py
+++ spryte_pack/app/main_window.py
@@ -4,6 +4,10 @@ from PyQt5.QtWidgets import (
     QWidget,
     QVBoxLayout,
 )
+from PyQt5.QtGui import (
+    QPainter,
+    QPixmap,
+)
 
 from spryte_pack.solver_env import SolverEnv
 
@@ -12,11 +16,21 @@ class SolverApp:
     def __init__(self):
         self._q_app = QApplication([])
         self._solver_env = SolverEnv()
+        self._solver_env.add_images(
+            self._solver_env.load_images_from_files(("../../assets/python-logo-transparent.png",)))
+        pixmap = QPixmap("../../assets/python-logo.png")
+        pixmap_t = QPixmap("../../assets/python-logo-transparent.png")
 
         self._window = QWidget()
-        self._layout = QVBoxLayout()
-        self._layout.addWidget(QLabel("Solver App"))
-        self._window.setLayout(self._layout)
+        layout = QVBoxLayout()
+        label = QLabel("Solver App")
+        label.setPixmap(pixmap_t)
+        layout.addWidget(label)
+        for i in self._solver_env._image_rects:
+            label_ = QLabel("Image")
+            label_.setPixmap(QPixmap.fromImage(i.qt_image))
+            layout.addWidget(label_)
+        self._window.setLayout(layout)
         self._window.show()
         self._q_app.exec_()
 
