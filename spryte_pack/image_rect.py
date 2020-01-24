1cc9fa177625884a44cbda3e01aaf189000958cc
Briggs <b3apollo13@gmail.com>
2020-01-24 11:50:36 -0600

    Added image transforms and caching

--- spryte_pack/image_rect.py
+++ spryte_pack/image_rect.py
@@ -30,29 +30,48 @@ class ImageRect(Rect):
 
     def __init__(self, image: Image.Image):
         super().__init__(image.width, image.height)
-        self.image: Image.Image = image
+        self._image: Image.Image = image
         self._rotation = ImageRect.Rotation.ROT_0
         self._filenames: List[str] = []
-        self.__cached_rotation = self._rotation
-        self.__cached_rotated_image = self.image
+        self._is_trimmed = True
         if hasattr(image, "filename"):
             self._filenames.append(image.filename)
 
+    @property
+    def unedited_image(self) -> Image.Image:
+        return self._image
+
+    @property
+    def image(self) -> Image.Image:
+        return self.get_transformed_image()
+
+    def get_maybe_trimmed_image(self):
+        return self.get_trimmed_image() if self._is_trimmed else self._image
+
     def get_trimmed_image(self) -> Image.Image:
-        bbox = self.image.getbbox()
-        return self.image.crop(bbox)
+        bbox = self._image.getbbox()
+        return self._image.crop(bbox)
 
     def trim_image(self):
-        self.image = self.get_trimmed_image()
+        self._image = self.get_trimmed_image()
 
-    def get_rotated_image(self) -> Image.Image:
-        if self.__cached_rotation != self._rotation:
-            self.__cached_rotation = self._rotation
-            self.__cached_rotated_image = self.image.transpose(self._rotation.transpose_op)
-        return self.__cached_rotated_image
+    def get_transformed_image(self) -> Image.Image:
+        if not hasattr(self.get_transformed_image, "__cached_rotation"):
+            self.get_transformed_image.__cached_rotation = None
+        if not hasattr(self.get_transformed_image, "__cached_is_trimmed"):
+            self.get_transformed_image.__cached_is_trimmed = None
+        if not hasattr(self.get_transformed_image, "__cached_rotated_image"):
+            self.get_transformed_image.__cached_rotated_image = None
+        if (self.get_transformed_image.__cached_rotation != self._rotation or
+                self.get_transformed_image.__cached_is_trimmed != self._is_trimmed):
+            self.get_transformed_image.__cached_rotation = self._rotation
+            self.get_transformed_image.__cached_is_trimmed = self._is_trimmed
+            self.get_transformed_image.__cached_rotated_image = self.get_maybe_trimmed_image().transpose(
+                self._rotation.transpose_op)
+        return self.get_transformed_image.__cached_rotated_image
 
     def apply_rotation_to_image(self):
-        self.image = self.get_rotated_image()
+        self._image = self.get_transformed_image()
 
     @staticmethod
     def images_same(image1: Image.Image, image2: Image.Image, trimmed: bool = True) -> bool:
@@ -63,10 +82,10 @@ class ImageRect(Rect):
         return bool(diff.getbbox())
 
     def image_same_as(self, other: Image.Image, trimmed: bool = True) -> bool:
-        return ImageRect.images_same(self.image, other, trimmed)
+        return ImageRect.images_same(self._image, other, trimmed)
 
     def __eq__(self, other: "ImageRect") -> bool:
-        return self.image_same_as(other.image, trimmed=False)
+        return self.image_same_as(other._image, trimmed=False)
 
     @property
     def rotation(self) -> Rotation:
