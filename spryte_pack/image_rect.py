8eac828ee394263858fca58ede14fbce21caef22
Briggs <b3apollo13@gmail.com>
2020-01-24 08:51:37 -0600

    Changed filenames to be list

--- spryte_pack/image_rect.py
+++ spryte_pack/image_rect.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from PIL import Image
 
 from spryte_pack.rect import Rect
@@ -7,6 +9,6 @@ class ImageRect(Rect):
     def __init__(self, image: Image.Image):
         super().__init__(image.width, image.height)
         self.image = image
-        self.filename = ""
+        self._filenames: List[str] = []
         if hasattr(image, "filename"):
-            self.filename = image.filename
+            self._filenames.append(image.filename)
