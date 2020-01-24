def9945a144130a018b745a718afd1bebe917478
Briggs <b3apollo13@gmail.com>
2020-01-24 12:50:45 -0600

    Added add_images() and load_images_from_files()

--- spryte_pack/solver_env.py
+++ spryte_pack/solver_env.py
@@ -1,4 +1,6 @@
-from typing import List
+from typing import List, Collection
+
+from PIL import Image
 
 from spryte_pack.image_rect import ImageRect
 
@@ -9,3 +11,10 @@ class SolverEnv:
     """
     def __init__(self):
         self._image_rects: List[ImageRect] = []
+
+    def add_images(self, images: Collection[Image.Image]):
+        self._image_rects.extend(tuple(ImageRect(image) for image in images))
+
+    @staticmethod
+    def load_images_from_files(paths: Collection[str]):
+        return tuple(Image.open(p) for p in paths)
