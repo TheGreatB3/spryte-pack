9dcea42bc403a909f9b740196c67f071064a8238
Briggs <b3apollo13@gmail.com>
2020-01-24 09:07:58 -0600

    Added image trimming

--- spryte_pack/image_rect.py
+++ spryte_pack/image_rect.py
@@ -8,7 +8,14 @@ from spryte_pack.rect import Rect
 class ImageRect(Rect):
     def __init__(self, image: Image.Image):
         super().__init__(image.width, image.height)
-        self.image = image
+        self.image: Image.Image = image
         self._filenames: List[str] = []
         if hasattr(image, "filename"):
             self._filenames.append(image.filename)
+
+    def get_trimmed_image(self) -> Image.Image:
+        bbox = self.image.getbbox()
+        return self.image.crop(bbox)
+
+    def trim_image(self):
+        self.image = self.get_trimmed_image()
