ba890559a7aeffde17af3879ee88105847dd5444
Briggs <b3apollo13@gmail.com>
2020-01-24 08:47:36 -0600

    Filled out ImageRect.__init__()

--- spryte_pack/image_rect.py
+++ spryte_pack/image_rect.py
@@ -1,6 +1,12 @@
 from PIL import Image
 
+from spryte_pack.rect import Rect
 
-class ImageRect:
-    def __init__(self):
-        pass
+
+class ImageRect(Rect):
+    def __init__(self, image: Image.Image):
+        super().__init__(image.width, image.height)
+        self.image = image
+        self.filename = ""
+        if hasattr(image, "filename"):
+            self.filename = image.filename
