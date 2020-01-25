4dc18000b3f1d7b0428f2650c3aca1b73a0be324
Briggs <b3apollo13@gmail.com>
2020-01-25 11:38:04 -0600

    Made SolverApp subclass QWidget

--- spryte_pack/app/main_window.py
+++ spryte_pack/app/main_window.py
@@ -13,30 +13,30 @@ from spryte_pack.solver_env import SolverEnv
 from spryte_pack.app.render_area import RenderArea
 
 
-class SolverApp:
+class SolverApp(QWidget):
     def __init__(self):
-        self._q_app = QApplication([])
+        super().__init__()
         self._solver_env = SolverEnv()
         self._solver_env.add_images(
             self._solver_env.load_images_from_files(("../../assets/python-logo-transparent.png",)))
         pixmap = QPixmap("../../assets/python-logo.png")
         pixmap_t = QPixmap("../../assets/python-logo-transparent.png")
 
-        self._window = QWidget()
         layout = QVBoxLayout()
-        render_area = RenderArea()
-        layout.addWidget(render_area)
+        self.render_area = RenderArea()
+        layout.addWidget(self.render_area)
         label = QLabel("Solver App")
         label.setPixmap(pixmap_t)
         layout.addWidget(label)
-        for i in self._solver_env.image_rects:
+        for image_rect in self._solver_env.image_rects:
             label_ = QLabel("Image")
-            label_.setPixmap(QPixmap.fromImage(i.qt_image))
+            label_.setPixmap(QPixmap.fromImage(image_rect.qt_image))
             layout.addWidget(label_)
-        self._window.setLayout(layout)
-        self._window.show()
-        self._q_app.exec_()
+        self.setLayout(layout)
 
 
 if __name__ == '__main__':
-    SolverApp()
+    q_app = QApplication([])
+    app = SolverApp()
+    app.show()
+    q_app.exec_()
